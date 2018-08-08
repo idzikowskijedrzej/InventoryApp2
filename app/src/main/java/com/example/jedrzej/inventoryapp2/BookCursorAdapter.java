d797a60bd9aebc47260b1fde6f014cf2cb5d9668
Jedrzej <idzikowskijedrzej@gmail.com>
2018-08-08 11:27:33 +0200

    added quantity_tv and made it display quantity value

--- app/src/main/java/com/example/jedrzej/inventoryapp2/BookCursorAdapter.java
+++ app/src/main/java/com/example/jedrzej/inventoryapp2/BookCursorAdapter.java
@@ -31,21 +31,23 @@ public class BookCursorAdapter extends CursorAdapter {
         TextView nameTextView = view.findViewById(R.id.name);
         TextView authorTextView = view.findViewById(R.id.author);
         TextView priceTextView = view.findViewById(R.id.price_tv);
+        TextView quantityTextView = view.findViewById(R.id.quantity_tv);
 
         // Find the columns of book attributes that we're interested in
         int nameColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_NAME);
         int authorColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_AUTHOR);
         int priceColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_PRICE);
+        int quantityColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_QUANTITY);
 
         // Read the book attributes from the Cursor
         String bookName = cursor.getString(nameColumnIndex);
         String bookAuthor = cursor.getString(authorColumnIndex);
         String bookPrice = cursor.getDouble(priceColumnIndex) + " $";
+        String bookQuantity = cursor.getInt(quantityColumnIndex) + " szt.";
 
         // If the book name or author is empty string or null
         // sue default text that says "Unknown breed", so the TextView isn't blank.
         if (TextUtils.isEmpty(bookAuthor)) {
-            bookName = context.getString(R.string.unknown_title);
             bookAuthor = context.getString(R.string.unknown_author);
         }
 
@@ -53,5 +55,6 @@ public class BookCursorAdapter extends CursorAdapter {
         nameTextView.setText(bookName);
         authorTextView.setText(bookAuthor);
         priceTextView.setText(bookPrice);
+        quantityTextView.setText(bookQuantity);
     }
 }
